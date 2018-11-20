7b76883441fc1ce6e47bd9493093907b1540b597
Eduardo de Santana Medeiros Alexandre <eduardo.ufpb@gmail.com>
2018-11-20 19:04:10 -0300

    Fazendo teste no travis também.

--- src/test/java/edu/ifpb/automacao/SireneAudioPadraoTest.java
+++ src/test/java/edu/ifpb/automacao/SireneAudioPadraoTest.java
@@ -4,9 +4,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
 class SireneAudioPadraoTest {
 
+	/* Esse teste não funciona no travis, pq não toca som lá.
+	 * Foi necessário desativar esse teste lá.
+	 * https://junit.org/junit5/docs/current/user-guide/ 
+	 * https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+	 */
+	@DisabledIfEnvironmentVariable(named = "TRAVIS", matches = "true")
 	@Tag("integração")
 	@Test
 	void testToca() {
